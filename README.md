e53b596ca5cec18f7a91d2e40439869db7f8a55b
Ola <ol.holovina@gmail.com>
2022-03-05 12:31:39 -0500

    browser sync add. check user add

---
+++ README.md
@@ -0,0 +1,6 @@
+#Project
+Web application inspired by Google photo album. This app uses Firebase and Bootstrap
+
+#To start the project locally
+npm install
+npm start
--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,8 @@
+function logError(error) {
+  alert(error.message);
+  console.log(error);
+}
+
 function getFirebaseModules(){
   const db = firebase.firestore();
   const storage = firebase.storage();
@@ -6,21 +11,20 @@ function getFirebaseModules(){
   return {db, storage, auth };
 }
 
+function getUser(){
+  const {auth} = getFirebaseModules();
+}
+
 function checkUser(){
-  return new Promise((resolve, reject) => {
-    document.addEventListener("DOMContentLoaded", function () {
-      const {auth} = getFirebaseModules();
+  document.addEventListener("DOMContentLoaded", function () {
+    const {auth} = getFirebaseModules();
 
-      auth.onAuthStateChanged((user) => {
-        if (user) {
-          window.location = '/gallery.html';
-          resolve(user);
-        } else {
-          window.location = '/index.html';
-          resolve(null);
-        }
-      })
-    });
-  })
-
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        console.log(user);
+      } else {
+        console.log('no user');
+      }
+    })
+  });
 }
